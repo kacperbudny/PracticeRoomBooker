1f3d6aea0de714f70448c57a8fb28227545491cf
Kacper Budny <kacperbudny98@gmail.com>
2023-11-03 19:43:13 +0100

    Begin working on login page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Form, Header, Segment } from "semantic-ui-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSubmit = () => {
+    console.log("test");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Segment>
+        <Header as="h2">Zaloguj się</Header>
+        <Form onSubmit={handleSubmit}>
+          <Form.Input label="E-mail" placeholder="E-mail" type="email" />
+          <Form.Input label="Hasło" placeholder="Hasło" type="password" />
+          <Button type="submit">Zaloguj się</Button>
+        </Form>
+      </Segment>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
