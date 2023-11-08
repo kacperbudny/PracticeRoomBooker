5e5fbf7df614eef8baf06af688e5d14e1e773a0c
Kacper Budny <kacperbudny98@gmail.com>
2023-11-08 19:19:58 +0100

    Style login page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,40 @@
+import { FormEvent } from "react";
 import { Button, Form, Header, Segment } from "semantic-ui-react";
+import styled from "styled-components";
 
 function App() {
-  const handleSubmit = () => {
-    console.log("test");
+  const handleSubmit = (e: FormEvent) => {
+    console.log(e);
   };
 
   return (
-    <>
-      <Segment>
-        <Header as="h2">Zaloguj się</Header>
-        <Form onSubmit={handleSubmit}>
-          <Form.Input label="E-mail" placeholder="E-mail" type="email" />
-          <Form.Input label="Hasło" placeholder="Hasło" type="password" />
-          <Button type="submit">Zaloguj się</Button>
-        </Form>
-      </Segment>
-    </>
+    <Center>
+      <WidthContainer>
+        <Segment padded>
+          <Header as="h2">Zaloguj się</Header>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input label="E-mail" placeholder="E-mail" type="email" />
+            <Form.Input label="Hasło" placeholder="Hasło" type="password" />
+            <Button type="submit" color="teal" fluid>
+              Zaloguj się
+            </Button>
+          </Form>
+        </Segment>
+      </WidthContainer>
+    </Center>
   );
 }
 
 export default App;
+
+const Center = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const WidthContainer = styled.div`
+  width: 100%;
+`;
