86d9caa16f8c5a9ad6b16e1c991fb7af874b4f0a
Kacper Budny <kacperbudny98@gmail.com>
2023-11-26 16:21:07 +0100

    Some more project configuration

--- packages/backend/src/api/routes/users.ts
+++ packages/backend/src/api/routes/users.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 
 export async function userRoutes(fastify: FastifyInstance) {
-  fastify.get("/login", async (req, res) => {
+  fastify.get("/login", { schema: {} }, async (req, res) => {
     return;
   });
 }
