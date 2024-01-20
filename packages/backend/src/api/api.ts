59a286158b6cc4cc133fe4e65080278932d36c28
Kacper Budny <kacperbudny98@gmail.com>
2024-01-20 12:41:08 +0100

    Fix ts-node-dev issues, add zod validator

--- packages/backend/src/api/api.ts
+++ packages/backend/src/api/api.ts
@@ -1,13 +1,24 @@
 import Fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
 import { dbDecorator } from "src/api/decorators/db-decorator";
 import { userRoutes } from "src/api/routes/users";
 
 const server = Fastify({ logger: true });
 
-server.register(async (instance, options) => {
-  dbDecorator(instance, options);
-  server.register(userRoutes);
-});
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server
+  .withTypeProvider<ZodTypeProvider>()
+  .register(async (instance, options) => {
+    dbDecorator(instance, options);
+    server.register(userRoutes);
+  });
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
