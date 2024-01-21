d4c49311d6a8546ee5519ebe3ce1e94801dd009b
Kacper Budny <kacperbudny98@gmail.com>
2024-01-21 12:54:11 +0100

    Begin setting up passport

--- packages/backend/src/api/api.ts
+++ packages/backend/src/api/api.ts
@@ -4,9 +4,12 @@ import {
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import z from "zod";
 import { dbDecorator } from "src/api/decorators/db-decorator";
 import { userRoutes } from "src/api/routes/users";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import { users } from "src/persistence/schema";
+import { eq } from "drizzle-orm";
 
 const server = Fastify({ logger: true });
 
@@ -19,6 +22,12 @@ server
     dbDecorator(instance, options);
     server.register(userRoutes);
   });
+
+passport.use(
+  new LocalStrategy(async (email, password, cb) => {
+    const result = await db.select().from(users).where(eq(users.email, email));
+  }),
+);
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
--- packages/backend/src/api/routes/users.ts
+++ packages/backend/src/api/routes/users.ts
@@ -1,8 +1,25 @@
 import { FastifyPluginAsync } from "fastify";
+import z from "zod";
 
 export const userRoutes: FastifyPluginAsync = async (fastify) => {
-  fastify.get("/login", { schema: {} }, async (req, res) => {
-    console.log(fastify.db);
-    return;
-  });
+  fastify.post(
+    "/login",
+    {
+      schema: {
+        body: z.object({
+          email: z.string().email(),
+          password: z.string().min(1).max(100),
+        }),
+        response: {
+          201: z.object({ token: z.string() }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { db } = fastify;
+
+      console.log(fastify.db);
+      return;
+    },
+  );
 };
