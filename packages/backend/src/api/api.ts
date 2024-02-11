ad67cedc969e7edd602cd168349be522d5507523
Kacper Budny <kacperbudny98@gmail.com>
2024-02-11 16:57:49 +0100

    Playing around with passport

--- packages/backend/src/api/api.ts
+++ packages/backend/src/api/api.ts
@@ -7,14 +7,54 @@ import {
 import { dbDecorator } from "src/api/decorators/db-decorator";
 import { userRoutes } from "src/api/routes/users";
 import passport from "passport";
-import LocalStrategy from "passport-local";
+import { Strategy as LocalStrategy } from "passport-local";
 import { users } from "src/persistence/schema";
 import { eq } from "drizzle-orm";
+import { db } from "src/persistence/persistence";
+import { pbkdf2, timingSafeEqual } from "node:crypto";
 
 const server = Fastify({ logger: true });
 
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
+
+passport.use(
+  new LocalStrategy(async (email, password, cb) => {
+    try {
+      const result = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email));
+
+      if (!result.length) {
+        return cb(null, false, { message: "Invalid credentials." });
+      }
+
+      const user = result[0];
+
+      pbkdf2(
+        password,
+        "test1234",
+        310000,
+        32,
+        "sha256",
+        (err, hashedPassword) => {
+          if (err) {
+            throw err;
+          }
+
+          if (!timingSafeEqual(user.password, hashedPassword)) {
+            return cb(null, false, { message: "Invalid credentials." });
+          }
+
+          return cb(null, user);
+        },
+      );
+    } catch (error) {
+      return cb(error);
+    }
+  }),
+);
 
 server
   .withTypeProvider<ZodTypeProvider>()
@@ -22,12 +62,6 @@ server
     dbDecorator(instance, options);
     server.register(userRoutes);
   });
-
-passport.use(
-  new LocalStrategy(async (email, password, cb) => {
-    const result = await db.select().from(users).where(eq(users.email, email));
-  }),
-);
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
